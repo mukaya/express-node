c67e153a8ae01ee21b6ca45a17adbe76a691f75e
Christian Lisangola <christian.lisangola@gmail.com>
2020-04-28 14:26:07 +0100

    POST new student

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ connection.connect((erreur) => {
 //Initialisation du serveur express
 const server = express();
 
+//Dire à express de mettre les données venants du formulaire dans BODY
+server.use(express.urlencoded({ extended: false }));
+
 //Dire à express où aller trouver les vues(Nos pages web que le user sait voir)
 server.set('views');
 
@@ -32,6 +35,31 @@ server.get('/apprenants', (req, res) => {
     if (erreur) throw erreur;
     return res.render('apprenants/index', { apprenants: resultat });
   });
+});
+
+server.post('/apprenants', (req, res) => {
+  console.log('BB');
+  connection.query(
+    `insert into students(nom,prenom) values('${req.body.nom}','${req.body.prenom}')`,
+    (erreur, resultat) => {
+      if (erreur) throw erreur;
+      return res.redirect('/apprenants');
+    }
+  );
+});
+
+server.get('/apprenants/new', (req, res) => {
+  return res.render('apprenants/new');
+});
+
+server.get('/apprenants/:id', (req, res) => {
+  connection.query(
+    `select * from students where id=${req.params.id}`,
+    (erreur, resultat) => {
+      if (erreur) throw erreur;
+      return res.render('apprenants/show', { apprenant: resultat[0] });
+    }
+  );
 });
 
 //Définition du port
