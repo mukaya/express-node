931d1fba709f6935ad3e08d8e708ff8c3228e012
Christian Lisangola <christian.lisangola@gmail.com>
2020-04-28 13:04:37 +0100

    Database connection configuration

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const ejs = require('ejs');
+const mysql = require('mysql');
+
+//Configuration de la base de données
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'kda_test',
+  password: '',
+});
+
+//Connexion à la base de données
+connection.connect((erreur) => {
+  if (erreur) {
+    throw erreur;
+  }
+  console.log('La connexion à la base de données est établie');
+});
 
 //Initialisation du serveur express
 const server = express();
 
+//Dire à express où aller trouver les vues(Nos pages web que le user sait voir)
+server.set('views');
+
+//Dire à express d'utiliser EJS comme moteur de template
+server.set('view engine', 'ejs');
+
 //GET
-server.get('/', (req, res) => {
-  return res.send('Bonjour KDA');
-});
 
 //Définition du port
-const PORT = 9000;
+const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Le serveur écoute sur le port ${PORT}`);
 });
